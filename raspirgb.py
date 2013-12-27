8c7fae4b16c74e1909964cb96112264f533028ee
Pavol Rusnak <stick@gk2.sk>
2013-12-27 17:36:07 +0100

    more foolproof

--- raspirgb.py
+++ raspirgb.py
@@ -1,5 +1,3 @@
-import smbus
-
 class RaspiRGB(object):
 
 	ADDR  = 0x61
@@ -8,13 +6,17 @@ class RaspiRGB(object):
 	PWM_B = 15
 
 	def __init__(self):
-		self.bus = smbus.SMBus(1)
-		self.bus.write_byte_data(self.ADDR, 0x00, 0x00)
-		self.bus.write_byte_data(self.ADDR, 0x14, 0xAA)
-		self.bus.write_byte_data(self.ADDR, 0x15, 0xAA)
-		self.bus.write_byte_data(self.ADDR, 0x16, 0xAA)
-		self.bus.write_byte_data(self.ADDR, 0x17, 0xAA)
-		self.set(0, 0, 0)
+		try:
+			import smbus
+			self.bus = smbus.SMBus(1)
+			self.bus.write_byte_data(self.ADDR, 0x00, 0x00)
+			self.bus.write_byte_data(self.ADDR, 0x14, 0xAA)
+			self.bus.write_byte_data(self.ADDR, 0x15, 0xAA)
+			self.bus.write_byte_data(self.ADDR, 0x16, 0xAA)
+			self.bus.write_byte_data(self.ADDR, 0x17, 0xAA)
+			self.set(0, 0, 0)
+		except:
+			self.bus = None
 
 	def set(self, r, g, b):
 		self.setr(r)
@@ -22,10 +24,13 @@ class RaspiRGB(object):
 		self.setb(b)
 
 	def setr(self, v):
-		self.bus.write_byte_data(self.ADDR, 2 + self.PWM_R, int(v))
+		if self.bus:
+			self.bus.write_byte_data(self.ADDR, 2 + self.PWM_R, int(v))
 
 	def setg(self, v):
-		self.bus.write_byte_data(self.ADDR, 2 + self.PWM_G, int(v))
+		if self.bus:
+			self.bus.write_byte_data(self.ADDR, 2 + self.PWM_G, int(v))
 
 	def setb(self, v):
-		self.bus.write_byte_data(self.ADDR, 2 + self.PWM_B, int(v))
+		if self.bus:
+			self.bus.write_byte_data(self.ADDR, 2 + self.PWM_B, int(v))
